7123fb870780170fc3c1b75c75d6b8c6a5bba0c4
ELDADBRHANO <ebrhano@gmail.com>
2022-06-01 20:34:58 +0300

    commit

---
+++ main.js
@@ -0,0 +1,103 @@
+// 1
+// var my_h1_id=document.getElementById("my_h1");
+// function printAlert() {
+//   alert("hello")
+// }
+
+// 2
+// var my_P=document.getElementById("myP")
+// function printEldad() {
+//   console.log("eldad");
+// }
+
+
+// 3
+// function change() {
+//   var mySpan=document.getElementById("me")
+//   mySpan.style.color="red";
+// }
+
+// 4
+// var h1_js= document.getElementById("my_h1");
+// function changeSizeOfH1(){
+//   h1_js.style.fontSize="120px"
+// }
+
+// 5
+// var change=document.getElementById("my_text");
+// function changeTextOfP(){
+//   change.innerText="i changed it"
+// }
+
+
+// 6
+// var getName=prompt("please enter your name.");
+// function printName(){
+//   document.write(getName)
+// }
+
+// 7
+// function printDate(){
+//   var giveDate=new Date();
+//   var areUTwelve= giveDate.getHours();
+//   var myP=document.getElementById("my_p")
+//   if(areUTwelve<=12){
+//     document.body.style.backgroundColor="yellow";
+//     myP.innerText="good day"
+//   }
+//   else{
+//     document.body.style.backgroundColor="blue";  
+//     myP.innerText="good evning"
+//   }
+// }
+
+
+// 8
+// function printAge(){
+//   var myText= document.getElementById("my_p")
+//   var userAge=+prompt("please enter your age.");
+//   if(userAge>18){
+//     myText.style.color="red"
+//     myText.innerText="welcome"
+//   }
+//   else{
+//     myText.style.color="blue"
+//     myText.innerText="not welcome"
+//   }
+// }
+
+// function changeColor(){
+//   var myText= document.getElementById("my_p")
+//   myText.style.color="green";
+// }
+
+
+
+// 9
+// function lotto() {
+//   var btn1=document.createElement("button")
+//   btn1.innerText="click"
+//   var rnd=Math.floor(Math.random()*57)
+//   document.body.append(btn1)
+//   var myArray=[]
+//   for(var i=0; i < 5; i++){
+//    var userNumber=+prompt("please enter number between 0-56");
+//     myArray.push(userNumber)
+//       btn1.onclick=function () {
+//         document.getElementById("my_p").innerText=myArray
+//         document.getElementById("my_d").innerHTML=rndNum
+
+//       }
+    
+//   }
+// }
+// lotto()
+
+
+
+// function grillNumber(){
+//   var rnd = Math.floor(Math.random()*57)
+//     document.write(rnd)
+  
+// }
+// grillNumber()
